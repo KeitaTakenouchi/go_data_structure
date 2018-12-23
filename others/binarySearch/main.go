831c1615ff89698140be6660a273c907d441579f
Takenouchi Keita <t.keita.osaka@gmail.com>
2018-12-24 01:51:52 +0900

    Remove a slice.

--- others/binarySearch/main.go
+++ others/binarySearch/main.go
@@ -6,18 +6,11 @@ import (
 )
 
 func main() {
-	values := []int{2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14}
-
-	for _, v := range values {
-		fmt.Println(v, divide143(v))
-	}
-
-	index := sort.Search(len(values), func(i int) bool {
-		return divide143(values[i])
+	index := sort.Search(100, func(i int) bool {
+		return divide143(i)
 	})
 
-	fmt.Println("index=", index, "value=", values[index])
-
+	fmt.Println("index=", index)
 }
 
 func divide143(n int) bool {
